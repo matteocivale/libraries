956ffcb368896d681cd380a45dc19071d2122d28
matteo_civale <matteo.civale@gmail.com>
2016-10-09 17:14:59 +0200

    Fixed animation parameter copy bug

--- comunication/command.cpp
+++ comunication/command.cpp
@@ -180,7 +180,7 @@
 			 
 			    /* Copy command parameter to animation memory */
 				for(i=0; i< PARAM_LEN-3; i++)
-					animation_memory[i] = myBuffer->cmdBuffer[myBuffer->headQueue].command.message.parameters[2 +i];
+					animation_memory[i] = myBuffer->cmdBuffer[myBuffer->headQueue].command.message.parameters[3 +i];
 			 
 			break;
       }
